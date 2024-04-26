cac7cfcce7a9e92545137586b2e704edf5088cd6
HoangDSR <54090585+hoang3402@users.noreply.github.com>
2024-04-26 17:13:46 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,14 +7,4 @@ export VITE_APP_CLIENT_PORT=${VITE_APP_CLIENT_PORT}
 export VITE_APP_ONE_ACCOUNT_EXTERNAL_ID=${VITE_APP_ONE_ACCOUNT_EXTERNAL_ID}
 
 cd server
-NODE_ENV=production nohup node index.js &
-
-cd ../client
-# mkdir -p dist
-
-# Build the UI in the background
-# nohup sh -c 'echo "The UI is rebuilding. Please wait..." && npm run build && echo "UI built successfully!" && cp -rf build/* dist/' &
-nohup sh -c 'echo "The UI is rebuilding. Please wait..." && npm run build && echo "UI built successfully!"' &
-
-# Serve the UI
-npm run preview
+NODE_ENV=production nohup node index.js 
